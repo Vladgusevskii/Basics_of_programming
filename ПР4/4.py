4b7a8dd160b586434f0044a64eef4eed80548688
Vladgusevskii <89155869136@mail.ru>
2023-10-28 22:20:14 +0300

    Ошибки

--- ПР4/4.py
+++ ПР4/4.py
@@ -1,9 +1,11 @@
 # -- coding: utf-8 --
-s = 0
-n = int(input('Введите кол-во чисел: '))
-for i in range(n):
-    a = int(input(f'Введите число №{i+1}: '))
-    s += a
-print (s)
+def main():
+    s = 0
+    n = int(input('Введите кол-во чисел: '))
+    for i in range(n):
+        a = int(input(f'Введите число №{i+1}: '))
+        s += a
+    print (s)
+main()
 
 
